24a553182433c66fb8fa121c076cb3751e5050c6
tdantonio <tdantonio@frba.utn.edu.ar>
2023-07-12 03:23:08 +0000

    restart_fs.sh lee block_count y block_size de superbloque.dat

--- scripts/restart_fs.sh
+++ scripts/restart_fs.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-cd ../fileSystem/archivos/
+cd fileSystem/archivos/
+
+# Elimino todo al carajo y lo vuelvo a crear
 rm bitmap.dat
 rm bloques.dat
 rm -r fcb/
 mkdir fcb
 touch bitmap.dat 
 touch bloques.dat
-truncate -s 4194304 bloques.dat
-truncate -s 65536 bitmap.dat
+
+# Leo los numeros del archivo "superbloque.dat"
+while IFS='=' read -r clave valor || [ -n "$clave" ]; do
+  if [[ "$clave" == "BLOCK_SIZE" ]]; then
+    block_size="${valor//[!0-9]/}"
+  elif [[ "$clave" == "BLOCK_COUNT" ]]; then
+    block_count="${valor//[!0-9]/}"
+  fi
+done < "superbloque.dat"
+
+# Verifico si los numeros fueron encontrados
+if [[ -z "$block_size" ]] || [[ -z "$block_count" ]]; then
+  echo "No se encontraron los números BLOCK_SIZE y BLOCK_COUNT en el archivo 'superbloque.dat'."
+  exit 1
+fi
+
+# Calculo tam_bloques
+tam_bloques=$((block_size * block_count))
+
+# Truncate con los tamaños correspondientes
+truncate -s "${block_count}" bitmap.dat
+truncate -s "${tam_bloques}" bloques.dat
+
+echo "bitmap.dat reseteado con tamaño ${block_count} Bytes"
+echo "bloques.dat reseteado con tamaño ${tam_bloques} Bytes"
--- scripts/set_ips.sh
+++ scripts/set_ips.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Verifico si se proporcionaron 4 argumentos de IP
+# Verifico si se recibieron 4 argumentos
 if [ "$#" -ne 4 ]; then
   echo "Se requieren 4 IPs como argumentos. Se reciben en orden: kernel, cpu, fs, memoria"
   exit 1
@@ -12,23 +12,22 @@ ip_cpu="$2"
 ip_fs="$3"
 ip_memoria="$4"
 
-
 # Array con los nombres de directorio
-directorios=("kernel" "cpu" "fileSystem" "memoria")
+modulos=("kernel" "cpu" "fileSystem" "memoria")
 
 # Array con los nombres de los archivos de configuración
 configs=("BASE" "DEADLOCK" "ERROR" "MEMORIA" "FS")
 
 # Recorro los directorios
-for dir in "${directorios[@]}"; do
-  config_dir="$dir/configs"
+for mod in "${modulos[@]}"; do
+  config_mod="$mod/configs"
 
-  # Recorro los archivos de configuración en el directorio
+  # Recorro los archivos de configuracion en el directorio
   for config in "${configs[@]}"; do
-    config_file="$config_dir/$config.config"
+    config_file="$config_mod/$config.config"
 
-    # Actualizo las líneas en el archivo de configuración
-    case $dir in
+    # Actualizo las lineas en el archivo de configuracion
+    case $mod in
       "kernel")
         sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$ip_kernel"/; 2,4s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/; 2,4s/IP_FILESYSTEM=.*/IP_FILESYSTEM="$ip_fs"/; 2,4s/IP_CPU=.*/IP_CPU="$ip_cpu"/" "$config_file"
         ;;
@@ -42,11 +41,11 @@ for dir in "${directorios[@]}"; do
         sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$ip_memoria"/" "$config_file"
         ;;
       *)
-        echo "Directorio no reconocido: $dir"
+        echo "Modulo no reconocido: $mod"
         ;;
     esac
 
     echo "IP actualizada en $config_file"
   done
-  echo "Modulo $dir actualizado."
+  echo "Modulo $mod actualizado."
 done
