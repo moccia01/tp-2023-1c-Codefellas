79a73171b371d4a5f09b42aa06a79cde8504909e
tdantonio <tdantonio@frba.utn.edu.ar>
2023-07-12 02:50:26 +0000

    agrego variable para cada ip

--- scripts/set_ips.sh
+++ scripts/set_ips.sh
@@ -6,6 +6,13 @@ if [ "$#" -ne 4 ]; then
   exit 1
 fi
 
+# Argumentos recibidos por parametro
+ip_kernel="$1"
+ip_cpu="$2"
+ip_fs="$3"
+ip_memoria="$4"
+
+
 # Array con los nombres de directorio
 directorios=("kernel" "cpu" "fileSystem" "memoria")
 
@@ -15,7 +22,7 @@ configs=("BASE" "DEADLOCK" "ERROR" "MEMORIA" "FS")
 # Recorro los directorios
 for dir in "${directorios[@]}"; do
   config_dir="$dir/configs"
-  
+
   # Recorro los archivos de configuración en el directorio
   for config in "${configs[@]}"; do
     config_file="$config_dir/$config.config"
@@ -23,16 +30,16 @@ for dir in "${directorios[@]}"; do
     # Actualizo las líneas en el archivo de configuración
     case $dir in
       "kernel")
-        sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$1"/; 2,4s/IP_MEMORIA=.*/IP_MEMORIA="$4"/; 2,4s/IP_FILESYSTEM=.*/IP_FILESYSTEM="$3"/; 2,4s/IP_CPU=.*/IP_CPU="$2"/" "$config_file"
+        sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$ip_kernel"/; 2,4s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/; 2,4s/IP_FILESYSTEM=.*/IP_FILESYSTEM="$ip_fs"/; 2,4s/IP_CPU=.*/IP_CPU="$ip_cpu"/" "$config_file"
         ;;
       "cpu")
-        sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$2"/; 2s/IP_MEMORIA=.*/IP_MEMORIA="$4"/" "$config_file"
+        sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$ip_cpu"/; 2s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/" "$config_file"
         ;;
       "fileSystem")
-        sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$3"/; 2s/IP_MEMORIA=.*/IP_MEMORIA="$4"/" "$config_file"
+        sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$ip_fs"/; 2s/IP_MEMORIA=.*/IP_MEMORIA="$ip_memoria"/" "$config_file"
         ;;
       "memoria")
-        sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$4"/" "$config_file"
+        sed -i "1s/IP_ESCUCHA=.*/IP_ESCUCHA="$ip_memoria"/" "$config_file"
         ;;
       *)
         echo "Directorio no reconocido: $dir"
